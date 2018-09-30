017f8cac698de156d10fbab38a8d5f20931b6858
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-01 01:46:03 +0300

    Add tournament mongoose model Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

---
+++ src/models/tournament.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const tournamentSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
+    team_count: {type: Number, required: true},
+    prize_pool: Number,
+    finished: {type: Boolean, default: false},
+    winner_team: {type: mongoose.Schema.Types.ObjectId, default: null},
+    teams: [
+        {
+            _id: mongoose.Schema.Types.ObjectId,
+            name: {type: String, required: true}
+        }
+    ]
+});
+
+module.exports = mongoose.model('Tournament', tournamentSchema);
