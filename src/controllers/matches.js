6a137678f5c9cf4bd2fb1fe010ad4df7ad27e1bb
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-09-30 23:17:00 +0300

    Replace default http requests with promise based axios client Update package.json
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/controllers/matches.js
+++ src/controllers/matches.js
@@ -1,23 +1,21 @@
 const Match = require('../models/match');
 const http = require('http');
 const mongoose = require('mongoose');
+const axios = require('axios');
 require('dotenv').config();
 
 exports.match_post_one = (req, res, next) => {
-    // fetching data from Steam API
-    http.get('http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=' +
-        process.env.STEAM_API_KEY + '&match_id=' + req.body.match_id, (resp) => {
-        let json = '';
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            json += chunk;
-        });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            const data = JSON.parse(json);
-            data.result._id = new mongoose.Types.ObjectId;
-            const match = new Match(data.result);
-
+    //fetching data from Steam API
+    axios.get('http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/', {
+        params: {
+            match_id: req.body.match_id,
+            key: process.env.STEAM_API_KEY
+        }
+    })
+        .then( resp => {
+            console.log(resp.data);
+            resp.data.result._id = new mongoose.Types.ObjectId;
+            const match = new Match(resp.data.result);
             match.save().then(result => {
                 res.status(201).json({
                     status: "ok",
@@ -31,13 +29,12 @@ exports.match_post_one = (req, res, next) => {
                     error: error
                 });
             });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                status: "error",
+                error: error
+            });
         });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        res.status(500).json({
-            status: "error",
-            error: err
-        });
-    })
 };
