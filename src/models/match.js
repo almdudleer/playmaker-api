2bf1fc15d86e1938c1b74488c73d8b712842e485
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-02 06:14:36 +0300

    Remove _id in matchSchema.playerSubdocument
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/models/match.js
+++ src/models/match.js
@@ -7,6 +7,7 @@ const matchSchema = mongoose.Schema({
     lobby_type: Number,
     players: [
         {
+            _id: false,
             account_id: Number,
             player_slot: {type: Number, required: true},
             hero_id: {type: Number, required: true}
