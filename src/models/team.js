8a19b3780b24d9e943a36c506fb548b3e1c3b93f
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-02 23:03:10 +0300

    Add team model
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

---
+++ src/models/team.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const teamSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
+    team_owner: mongoose.Schema.Types.ObjectId,
+    players: {
+        type: [
+            {
+                _id: false,
+                account_id: {type: Number, required: true}
+            }
+        ],
+    }
+});
+
+module.exports = mongoose.model('Team', teamSchema);
