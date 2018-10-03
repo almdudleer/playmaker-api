c2379bffc9747156d1ff29aee1e9274ca94be944
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-03 16:59:48 +0300

    В модель турнира добавлен владелец
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/models/tournament.js
+++ src/models/tournament.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const tournamentSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    owner: {type: mongoose.Schema.Types.ObjectId, required: true},
     name: {type: String, required: true, unique: true},
     team_count: {type: Number, required: true},
     prize_pool: Number,
