e13a57ec83674d9e5d3027309b6997b76037a19a
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-03 16:08:19 +0300

    Добавлен DELETE запрос на /tournaments/tournamentId/teams Удаление команды из списка участников
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/controllers/tournaments.js
+++ src/controllers/tournaments.js
@@ -79,7 +79,7 @@ exports.tournament_add_team = (req, res, next) => {
     Team.findById(req.body.teamId)
         .exec()
         .then(team => {
-            if (team) { //Если существиет команда с передаваемым Id
+            if (team) { //Если существует команда с передаваемым Id
                 Tournament.findOneAndUpdate(
                     {_id: req.params.tournamentId},
                     {$addToSet: {teams: {_id: team._id, name: team.name}}}, //Если команда уже есть, ничего не изменится
@@ -105,5 +105,36 @@ exports.tournament_add_team = (req, res, next) => {
         .catch(err => {
             res.status(500).json({error: err})
         });
+};
 
+exports.tournament_delete_team = (req, res, next) => {
+    Team.findById(req.body.teamId)
+        .exec()
+        .then(team => {
+            if (team) { //Если существует команда с передаваемым Id
+                Tournament.findOneAndUpdate(
+                    {_id: req.params.tournamentId},
+                    {$pull: {teams: {_id: team._id}}}, //Удаляем команду с турнира
+                    {new: true}
+                )
+                    .exec()
+                    .then(doc => {
+                        const response = {
+                            status: "ok",
+                            removedTeam: team,
+                            updatedTournament: doc
+                        };
+                        res.status(200).json(response);
+                    })
+                    .catch(err => {
+                        res.status(500).json({error: err})
+                    })
+            } else return res.status(404).json({
+                status: "error",
+                message: "team not found"
+            })
+        })
+        .catch(err => {
+            res.status(500).json({error: err})
+        });
 };
--- src/routes/tournaments.js
+++ src/routes/tournaments.js
@@ -12,4 +12,6 @@ router.delete('/', TournamentsController.tournament_delete_one);
 
 router.post('/:tournamentId/teams', TournamentsController.tournament_add_team);
 
+router.delete('/:tournamentId/teams', TournamentsController.tournament_delete_team);
+
 module.exports = router;
