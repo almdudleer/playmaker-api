b2a612d294f063d331d47f18d2662552267fc07e
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-03 16:07:09 +0300

    В модели команды имя сделано уникальным
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/models/team.js
+++ src/models/team.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const teamSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
+    name: {type: String, required: true, unique: true},
     team_owner: mongoose.Schema.Types.ObjectId,
     players: {
         type: [
