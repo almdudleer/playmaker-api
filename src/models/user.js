b32a987c94b618cdbc10ed27cdbc524f861539d5
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-08 10:58:02 +0300

    Обновлена модель пользователя
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,9 @@ const userSchema = mongoose.Schema({
     nickname: {type: String, required: true, unique:true},
     email: {type: String, required: true, unique:true},
     password: {type: String, required: true},
-    account_id: {type: Number, unique: true}
+    account_id: {type: Number, unique: true},
+    fav_tournaments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tournament' }],
+    fav_teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tournament' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
