fd680104d384d28c15ac8c807c78fd0c18e508c5
Uchvatov Semyon <s-uchvatov@yandex.ru>
2018-10-22 10:25:45 +0300

    Добавлен роутер на /user
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const matchesRouter = require('./src/routes/matches');
 const tournamentsRouter = require('./src/routes/tournaments');
 const teamRouter = require('./src/routes/teams');
+const userRouter = require('./src/routes/users');
 require('dotenv').config();
 
 
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 app.use('/matches', matchesRouter);
 app.use('/tournaments', tournamentsRouter);
 app.use('/teams', teamRouter);
+app.use('/user', userRouter);
 //test helper
 app.use('/test', require("./src/routes/test"));
 
