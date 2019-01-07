1366a9ac47f58ef5565f2a3b7466e2b3fd24f75a
Uchvatov Semyon <s-uchvatov@yandex.ru>
2019-01-07 19:29:00 +0300

    добавлены имена героев для получени иконок
    Signed-off-by: Uchvatov Semyon <s-uchvatov@yandex.ru>

--- src/steamEndpoints.js
+++ src/steamEndpoints.js
@@ -1,3 +1,4 @@
 module.exports = {
-    getMatchDetails: "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/"
+    getMatchDetails: "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/",
+    smallHeroIcon: "http://media.steampowered.com/apps/dota2/images/heroes/"
 };
